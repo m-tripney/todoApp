97ea58c96ec7c4c3ae97cbcb392bbf7d7cc8d71e
Mark Tripney <mark.tripney@gmail.com>
2019-05-07 14:21:11 +0200

    Add objects to todoList, not just text; add 'Completed' status to todo items.

--- todoApp.js
+++ todoApp.js
@@ -1,21 +1,29 @@
 let todoList = {
-    todos: ['Item 1', 'Item 2', 'Item 3'],
+    todos: [],
     displayTodos: function() {
         console.log('My Todos:');
         for (let i = 0; i < this.todos.length; i++) {
             console.log(this.todos[i]);
         }
     },
-    addTodo: function(todo) {
-        this.todos.push(todo);
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
         this.displayTodos();
     },
-    changeTodo: function(position, newValue) {
-        this.todos[position] = newValue;
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
         this.displayTodos();
     }
 };
@@ -24,7 +32,6 @@ let todoList = {
 TEST CODE
 */
 
-// todoList.displayTodos();
-// todoList.addTodo('Item 4');
+// todoList.addTodo('Item 1');
 // todoList.changeTodo(0, 'First Item');
-// todoList.deleteTodo(0);
+// todoList.toggleCompleted(0);
