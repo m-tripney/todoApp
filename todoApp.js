0a728d5612e7a2c2802b21b4e3fe5296f764da32
Mark Tripney <mark.tripney@gmail.com>
2019-05-07 13:20:59 +0200

    Move functions onto an object.

--- todoApp.js
+++ todoApp.js
@@ -1,26 +1,30 @@
-function displayTodos() {
-    console.log('My Todos:');
-    for (let i = 0; i < todos.length; i++) {
-        console.log(todos[i]);
+let todoList = {
+    todos: ['Item 1', 'Item 2', 'Item 3'],
+    displayTodos: function() {
+        console.log('My Todos:');
+        for (let i = 0; i < this.todos.length; i++) {
+            console.log(this.todos[i]);
+        }
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
     }
-}
+};
 
-function addTodo(todo) {
-    todos.push(todo);
-    displayTodos();
-}
+/*
+TEST CODE
+*/
 
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos();
-}
-
-function deleteTodo(position) {
-    todos.splice(position, 1);
-    displayTodos();
-}
-
-let todos = ['Item 1', 'Item 2', 'Item 3'];
-addTodo('Item 4');
-changeTodo(0, 'First Item');
-deleteTodo(3);
+// todoList.displayTodos();
+// todoList.addTodo('Item 4');
+// todoList.changeTodo(0, 'First Item');
+// todoList.deleteTodo(0);
